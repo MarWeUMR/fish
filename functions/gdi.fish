d7ba5239aed5c916bbd797c6bfae37635af54143
Marcus Weber <weberma@students.uni-marburg.de>
2023-02-02 20:44:13 +0100

    fix: change order of args. now diff color fits better

--- functions/gdi.fish
+++ functions/gdi.fish
@@ -1,5 +1,7 @@
 # TODO:
-# add preview for commit content while selecting interactively
+# add preview for commit content while selecting interactively, if it even helps...
+# set better delta options (diff-so-fancy)
+# make diff layout monitor dependend
 
 # interactively search in git logs for two commits to diff
 function gdi
@@ -27,7 +29,7 @@ function gdi
     set GIT_DIFF_ARG_TWO (string split " " $COMMIT_TWO_SPLIT[1])
 
     if test (string length $GIT_DIFF_ARG_ONE[-1]) -gt 6; and test (string length $GIT_DIFF_ARG_TWO[-1]) -gt 6
-        git diff $GIT_DIFF_ARG_ONE[-1] $GIT_DIFF_ARG_TWO[-1]
+        git diff $GIT_DIFF_ARG_TWO[-1] $GIT_DIFF_ARG_ONE[-1]
     else
         echo Something is wrong with the commit hashes
     end
