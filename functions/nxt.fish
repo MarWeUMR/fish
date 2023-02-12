ac029a104030af49094023b985b46739a4d8cd2b
Marcus Weber <weberma@students.uni-marburg.de>
2023-02-12 21:12:22 +0100

    feature: nxt now with multiselect and printing the command for c&p

--- functions/nxt.fish
+++ functions/nxt.fish
@@ -5,13 +5,14 @@ function nxt
     # get a list of all tests and format it such that fzf can be used.
     set LIST_OF_TESTS (cargo nextest list --all-features)
     # xargs -n 1 is used to make a fzf compatible list of the items from above
-    set SELECTED_TEST (echo $LIST_OF_TESTS | xargs -n 1 | fzf --ansi -i)
+    set SELECTED_TEST (echo $LIST_OF_TESTS | xargs -n 1 | fzf --ansi -i -m)
 
 
     if test -z "$SELECTED_TEST"
         return
     else
         cargo nextest run --all-features $SELECTED_TEST --no-capture
+        echo cargo nextest run --all-features $SELECTED_TEST --no-capture
     end
 
 end
